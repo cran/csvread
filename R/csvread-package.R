7a44c6f79096d5ea46b2bc5f8aa6279c3ca560e9
Sergei Izrailev <sizrailev@jabiruventures.com>
2015-03-08 00:00:00 +0000

    version 1.2

--- R/csvread-package.R
+++ R/csvread-package.R
@@ -4,9 +4,10 @@
 #
 # General description 
 # 
-# Sergei Izrailev, 2011-2014
+# Sergei Izrailev, 2011-2015
 #-------------------------------------------------------------------------------
 # Copyright 2011-2014 Collective, Inc.
+# Copyright 2015 Jabiru Ventures LLC
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,12 +31,12 @@
 #' @title Fast Specialized CSV File Loader. 
 #' @author Sergei Izrailev
 #' @section Maintainer: Sergei Izrailev 
-#' @section Copyright: Copyright (C) Collective, Inc.
+#' @section Copyright: Copyright (C) Collective, Inc.; with portions Copyright (C) Jabiru Ventures LLC
 #' @section License: Apache License, Version 2.0, 
 #'    available at http://www.apache.org/licenses/LICENSE-2.0
-#' @section URL: http://github.com/collectivemedia/csvread
+#' @section URL: http://github.com/jabiru/csvread
 #' @section Installation from github: 
-#' \code{devtools::install_github("collectivemedia/csvread")}
+#' \code{devtools::install_github("jabiru/csvread")}
 #' @keywords csv delimited file read.csv bigint 64-bit integer64
 #' @useDynLib csvread
 #' @exportPattern "*"
